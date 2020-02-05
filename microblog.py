4b490b6764f7ab5950bff743d1b5d398ccd363a1
Dre mf Randaci <drerandaci@gmail.com>
2020-02-05 13:06:02 -0600

    app shell context processor config, end of chapter

--- microblog.py
+++ microblog.py
@@ -1,2 +1,8 @@
 # import the app variable that is a member of the app package
 from app import app
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
